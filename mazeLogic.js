c05f8848116f7e5f04877b719d45145505cd7d17
Adam Horak <adam@thehoraks.com>
2014-05-21 09:41:28 -0700

    refactored to have maze be an export of mazeLogic

--- mazeLogic.js
+++ mazeLogic.js
@@ -1,4 +1,5 @@
-module.exports = {};
+var _ = require('underscore');
+
 
 var addDoorCount = function(room) {
   var doors = 0;
@@ -14,21 +15,41 @@ var addDoorCount = function(room) {
   return room;
 };
 
-module.exports.init = function(maze) {
-	var roomWithDoorCount = {};
-  maze.rooms.forEach(function(room) {
-		if(room.entrance) {
-      roomWithDoorCount = addDoorCount(room);
-		}
+module.exports.init = function(mazeFileName) {
+	var mazeWithDoorCount = require(mazeFileName);
+  var startRoom = '';
+  var endRoom = '';
+
+  var roomWithDoorCount = {};
+  mazeWithDoorCount.rooms.forEach(function(room) {
+    roomWithDoorCount = addDoorCount(room);
+    if(room.entrance) {
+      startRoom = room.name;
+    }
+    if(room.treasure) {
+      endRoom = room.name;
+    }
 	});
-  return roomWithDoorCount;
+
+  module.exports.maze = mazeWithDoorCount;
+  module.exports.maze.startRoom = startRoom;
+  module.exports.maze.endRoom = endRoom;
 };
 
-module.exports.nextRoom = function(currentRoom, direction, maze) {
+module.exports.start = function() {
+  var startRoom = {};
+  startRoom = _.find(exports.maze.rooms, function(obj) {
+    return(obj.name === module.exports.maze.startRoom);
+  });
+
+  return startRoom;
+};
+
+module.exports.nextRoom = function(currentRoom, direction) {
   var newRoomLetter = currentRoom[direction];
   var newRoom = {};
 
-  maze.rooms.forEach(function(room) {
+  module.exports.maze.rooms.forEach(function(room) {
     if(room.name === newRoomLetter) {
       newRoom = room;
       addDoorCount(newRoom);
